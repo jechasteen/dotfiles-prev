08444b8d2fdf531e6adc6606912d1e31ff5c352b
Jonathan Chasteen <jonathan.chasteen@live.com>
2019-12-06 15:31:57 -0500

    Pull changes from monster awesome/autostart.sh and .gitignore

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -7,10 +7,13 @@ function run {
 	fi	
 }
 
-compton -bcf -D 8
+picom -b
+
 TRACKPOINT_ID=$(xinput list | grep -i trackpoint | cut -f2 | sed 's/id=//')
 xinput set-prop $TRACKPOINT_ID 275 -0.2
-run nm-applet &
+xset r rate 400 40
+
+#run nm-applet &
 run volumeicon &
-run cbatticon &
+#run cbatticon &
 albert &
